4c31022c8b5de44b2c70dc4c0061c7b0933e8a5d
Сергей Мильгуй <112504417+Milja1@users.noreply.github.com>
2022-10-02 17:01:28 +0300

    change 2

--- ДЗ_hw_3/scripts10.js
+++ ДЗ_hw_3/scripts10.js
@@ -1,11 +1,32 @@
-let t = 6789;
-document.write(typeof n);
-document.write('<br>')
-number = t.toString();
-document.write(number);
-document.write('<br>')
-document.write(`Количество цифр в числе: ${number.length}.`);
-document.write('<br>')
-document.write(`Сумма цифр: ${+number[0] + +number[1] + +number[2] + +number[3]}.`);
-document.write('<br>')
-document.write(number[3] + +number[2] + +number[1] + +number[0]);
+let t = 6987502146801258;
+number = String(t);
+len = number.length;
+numbers = 0;
+sum = 0;
+rev = 0;
+digits = 0;
+
+document.write(`Число состоит из цифр: `)
+for (let i = 0; i < len; i++){
+	numbers = number[i];
+	document.write(`${numbers} `)
+}
+
+document.write(`<br>Cумма цифр в числе: `)
+for (let i = 0; i < len; i++){
+	sum += +number[i];
+}
+document.write(`${sum} `)
+
+document.write(`<br>Обратный порядок цифр в числе: `);
+for (let i = (len - 1); i > -1; i--){
+	x = number[i];
+	document.write(`${x} `);
+}
+
+document.write(`<br>Количество цифр в числе: `);
+for (let i = 0; i < len; i++){
+	number /= 10;
+	digits++;
+}
+document.write(`${digits}`);
