50adbefbe330e778633884efeb45c70a7b5b6a68
Сергей Мильгуй <112504417+Milja1@users.noreply.github.com>
2022-10-16 15:27:33 +0300

    homework 6 correction 3

--- ДЗ hw_lesson_6/js/scripts10.js
+++ ДЗ hw_lesson_6/js/scripts10.js
@@ -4,50 +4,50 @@ function dismanUrl(url){
 	options = url.indexOf('?');
 	hash = url.indexOf('#');
 	while (true) {
-	switch (true) {
-	  case (hash === -1 && options === -1 && address === -1):
-		arrUrl.push(url);
-		console.log(arrUrl);
-		document.write(`[${arrUrl}]<br>`);
-		break;
-	  case (hash === -1 && options === -1):
-		arrUrl.push(url.slice(0, address), url.slice(address));
-		console.log(arrUrl);
-		document.write(`[${arrUrl}]<br>`);
-		break;
-	  case (address === -1 && options === -1):
-		arrUrl.push(url.slice(0, hash), url.slice(hash));
-		console.log(arrUrl);
-		document.write(`[${arrUrl}]<br>`);
-		break;
-	  case (hash === -1 && address === -1):
-		arrUrl.push(url.slice(0, options), url.slice(options));
-		console.log(arrUrl);
-		document.write(`[${arrUrl}]<br>`);
-		break;
-	  case (hash === -1):
-		arrUrl.push(url.slice(0, address), url.slice(address, options), url.slice(options));
-		console.log(arrUrl);
-		document.write(`[${arrUrl}]<br>`);
-		break;
-	  case (options === -1):
-		arrUrl.push(url.slice(0, address), url.slice(address, hash), url.slice(hash));
-		document.write(`[${arrUrl}]<br>`);
-		console.log(arrUrl);
-		break;  
-	  case (address === -1):
-		arrUrl.push(url.slice(0, options), url.slice(options, hash), url.slice(hash));
-		console.log(arrUrl);
-		document.write(`[${arrUrl}]<br>`);
-		break;
-	  case true: 
-		arrUrl.push(url.slice(0, address), url.slice(address, options), url.slice(options, hash), url.slice(hash));
-		console.log(arrUrl);
-		document.write(`[${arrUrl}]<br>`);
+		switch (true) {
+			case (hash === -1 && options === -1 && address === -1):
+				arrUrl.push(url);
+				console.log(arrUrl);
+				document.write(`[${arrUrl}]<br>`);
+				break;
+			case (hash === -1 && options === -1):
+				arrUrl.push(url.slice(0, address), url.slice(address));
+				console.log(arrUrl);
+				document.write(`[${arrUrl}]<br>`);
+				break;
+			case (address === -1 && options === -1):
+				arrUrl.push(url.slice(0, hash), url.slice(hash));
+				console.log(arrUrl);
+				document.write(`[${arrUrl}]<br>`);
+				break;
+			case (hash === -1 && address === -1):
+				arrUrl.push(url.slice(0, options), url.slice(options));
+				console.log(arrUrl);
+				document.write(`[${arrUrl}]<br>`);
+				break;
+			case (hash === -1):
+				arrUrl.push(url.slice(0, address), url.slice(address, options), url.slice(options));
+				console.log(arrUrl);
+				document.write(`[${arrUrl}]<br>`);
+				break;
+			case (options === -1):
+				arrUrl.push(url.slice(0, address), url.slice(address, hash), url.slice(hash));
+				document.write(`[${arrUrl}]<br>`);
+				console.log(arrUrl);
+				break;  
+			case (address === -1):
+				arrUrl.push(url.slice(0, options), url.slice(options, hash), url.slice(hash));
+				console.log(arrUrl);
+				document.write(`[${arrUrl}]<br>`);
+				break;
+			case true: 
+				arrUrl.push(url.slice(0, address), url.slice(address, options), url.slice(options, hash), url.slice(hash));
+				console.log(arrUrl);
+				document.write(`[${arrUrl}]<br>`);
+				break;
+		}
 		break;
 	}
-	break;
-}
   }
   dismanUrl('https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3');
   dismanUrl('https://tech.onliner.by/2018?utm');
