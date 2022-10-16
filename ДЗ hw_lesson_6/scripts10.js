eb5e94096a91f89501c4442c8ea9f4fb6e4542bb
Сергей Мильгуй <112504417+Milja1@users.noreply.github.com>
2022-10-16 14:50:52 +0300

    homework 6 corrections 1

--- ДЗ hw_lesson_6/scripts10.js
+++ ДЗ hw_lesson_6/scripts10.js
@@ -3,54 +3,57 @@ function dismanUrl(url){
 	address = url.indexOf('/', 8);
 	options = url.indexOf('?');
 	hash = url.indexOf('#');
+	while (true) {
 	switch (true) {
 	  case (hash === -1 && options === -1 && address === -1):
 		arrUrl.push(url);
 		console.log(arrUrl);
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		break;
 	  case (hash === -1 && options === -1):
 		arrUrl.push(url.slice(0, address), url.slice(address));
 		console.log(arrUrl);
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		break;
 	  case (address === -1 && options === -1):
 		arrUrl.push(url.slice(0, hash), url.slice(hash));
 		console.log(arrUrl);
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		break;
 	  case (hash === -1 && address === -1):
 		arrUrl.push(url.slice(0, options), url.slice(options));
 		console.log(arrUrl);
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		break;
 	  case (hash === -1):
 		arrUrl.push(url.slice(0, address), url.slice(address, options), url.slice(options));
 		console.log(arrUrl);
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		break;
 	  case (options === -1):
 		arrUrl.push(url.slice(0, address), url.slice(address, hash), url.slice(hash));
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		console.log(arrUrl);
 		break;  
 	  case (address === -1):
 		arrUrl.push(url.slice(0, options), url.slice(options, hash), url.slice(hash));
 		console.log(arrUrl);
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		break;
 	  case true: 
 		arrUrl.push(url.slice(0, address), url.slice(address, options), url.slice(options, hash), url.slice(hash));
 		console.log(arrUrl);
-		document.write(`[${arrUrl}]`);
+		document.write(`[${arrUrl}]<br>`);
 		break;
 	}
+	break;
+}
   }
   dismanUrl('https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3');
-  //dismanUrl('https://tech.onliner.by/2018?utm');
-  //dismanUrl('https://tech.onliner.by/2018');
-  //dismanUrl('https://tech.onliner.by');
-  //dismanUrl('https://tech.onliner.by/2018#zag3');
-  //dismanUrl('https://tech.onliner.by#zag3');
-  //dismanUrl('https://tech.onliner.by?utm#zag3');
-  //dismanUrl('https://tech.onliner.by?utm');
+  dismanUrl('https://tech.onliner.by/2018?utm');
+  dismanUrl('https://tech.onliner.by/2018');
+  dismanUrl('https://tech.onliner.by');
+  dismanUrl('https://tech.onliner.by/2018#zag3');
+  dismanUrl('https://tech.onliner.by#zag3');
+  dismanUrl('https://tech.onliner.by?utm#zag3');
+  dismanUrl('https://tech.onliner.by?utm');
